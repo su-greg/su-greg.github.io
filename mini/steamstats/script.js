cb8e87e84aec3bed2f7940696a07252f1e2cb62d
greg <52139156+su-greg@users.noreply.github.com>
2020-04-04 20:00:21 +0100

    Add files via upload

--- mini/steamstats/script.js
+++ mini/steamstats/script.js
@@ -8,8 +8,8 @@ $("#form").submit(function(e) {
 $(window).on('hashchange', () => {
 
     const appid = window.location.hash.replace("#", "");                                // Change the hash to the entered appid
-    const api = "https://api.steampowered.com/";    // Cors work around for steam's web api
-    const steam = "https://store.steampowered.com/app/";    // Cors work around for steam store
+    const api = "https://cors-anywhere.herokuapp.com/https://api.steampowered.com/";    // Cors work around for steam's web api
+    const steam = "https://cors-anywhere.herokuapp.com/https://store.steampowered.com/app/";    // Cors work around for steam store
 
     if (!appid) {       // If the appid is blank ...
 
@@ -28,10 +28,26 @@ $(window).on('hashchange', () => {
             $("#loader").css("display", "inherit");     // Show loading animation
         });
 
+        /*/ Bypass age restriction notice
+        var sessionid = $.get( (steam + 105600) , function(data) {
+            sessionid = data.split('var g_sessionID = "').pop().split('";')[0];
+        });
+
+        sessionid.done( function() {
+            console.log(sessionid);
+            $.post("https://cors-anywhere.herokuapp.com/https://store.steampowered.com/agecheckset/app/"+appid,
+            { sessionid: sessionid,ageDay: "1",ageMonth: "October",ageYear: "2000" }
+            ).done( function( res ) {
+
+                if ( res.success == 1) console.log("authenticated!")
+                else console.log("failed to auth!")
+            });
+        });*/
+
         // The appid doesn't exist in the Steam API
-        request.fail(function(e) {
-            console.log( e );
-            $("#appid").css("border-color", "#f00");
+        request.fail( function(e) {
+            console.log( "App doesnt exist" );
+            document.getElementById("appid").style.animation = "red 0.3s ease 2"
         });
 
         // The appid does exist in the Steam API
@@ -40,8 +56,6 @@ $(window).on('hashchange', () => {
             var collected = 0;
             var failed = 0;
 
-            $("#appid").css("border-color", "#000");
-
             // Display stats once collected
             function displayStats($) {
                 if (collected == 2) {   // If no errors ...
@@ -49,9 +63,12 @@ $(window).on('hashchange', () => {
                     document.title = "SteamStats - "+ stats.title;
                     $("#title").html(stats.title);
                     $("#description").html(stats.desc);
+                    $("#reviews").html(stats.reviews);
+                    $("#release").html("Released "+stats.released);
+                    $("#image").attr("src", stats.img);
                 
                     $("#loader").css("display", "none");     // Hide loading animation
-                    $("#results").css("display", "inherit");     // Hide loading animation
+                    $("#results").css("display", "inline-grid");     // Hide loading animation
                     
                     console.log(stats);
                 } else if (!!failed) {  // If errors ...
@@ -68,13 +85,20 @@ $(window).on('hashchange', () => {
 
             // Request steam store page ( Web Scraping )
             $.get( (steam + appid) , function(data) {
+                
+                if (data.includes("agecheckset")) {
+                    stats.title = data.split('<title>').pop().split(' on Steam</title>')[0];                                                // Scrape Title
+                    stats.desc = "Content is age restricted! ( I'm working on a workaround for this but it's <em>really</em> tricky! )";
+                } else {
+                    stats.reviews = data.split('style="cursor: pointer;" data-tooltip-html="').pop().split('"')[0];       // Scrape Reviews
+                    stats.devs = data.split('<div class="summary column" id="developers_list">').pop().split('</div>')[0];                  // Scrape Developers
+                    stats.price = data.split('<div class="game_purchase_price price">').pop().split('</div>')[0];                           // Scrape Price
+                    stats.desc = data.split('<div class="game_description_snippet">').pop().split('</div>')[0];                             // Scrape Description
+                    stats.img = data.split('class="game_header_image_full" src="').pop().split('"')[0];                                     // Scrape Image
+                    stats.title = data.split('<div class="apphub_AppName">').pop().split('</div>')[0];                                      // Scrape Title
+                    stats.released = data.split('<div class="date">').pop().split('</div>')[0];                                             // Scrape Release date
+                }
 
-                stats.reviews = data.split('<span class="game_review_summary positive" data-tooltip-html="').pop().split('"')[0];       // Scrape reviews
-                stats.devs = data.split('<div class="summary column" id="developers_list">').pop().split('</div>')[0];                  // Scrape Developers
-                stats.price = data.split('<div class="game_purchase_price price">').pop().split('</div>')[0];                           // Scrape Price
-                stats.desc = data.split('<div class="game_description_snippet">').pop().split('</div>')[0];                             // Scrape Description
-                stats.released = data.split('<div class="date">').pop().split('</div>')[0];                                             // Scrape Release date
-                stats.title = data.split('<title>').pop().split(' on Steam</title>')[0];                                                // Scrape title
 
                 collected++;
                 displayStats($)
@@ -82,3 +106,7 @@ $(window).on('hashchange', () => {
         });
     }
 });
+
+if (!!window.location.hash) {
+    $(window).trigger("hashchange");
+}
